4c5451efbb31619e7391729e2d9ca02b9aeb7ac4
Lalolalo85 <luis.avila8532@gmail.com>
2018-01-22 19:36:41 -0600

    Finished 6's with ES6

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
 class Grub {
+  // Constructor
+  constructor(){
+    this.age = 0;
+    this.color = "pink";
+    this.food = "jelly";
+  };
+
+  // Functions
+  eat(){
+    return "Mmmmmmmmm jelly"
+  };
+
 };
